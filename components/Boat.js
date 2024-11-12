db0d07613a4aaa0d0b6398697fdbdb75ac865597
RPWeiQiang <23013990@myrp.edu.sg>
2024-11-12 15:16:19 +0800

    boats

--- components/Boat.js
+++ components/Boat.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import { Text, View, Image,ScrollView,} from 'react-native';
+import { Text, View, Image,ScrollView,StyleSheet} from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome6";
 
+const styles = StyleSheet.create({
+    container: {
+        alignItems:'center',
+        backgroundColor:'#d3d3d3',
+        marginTop:30,
+        paddingTop:40,
+        paddingBottom:20,
+        margin:10,
+    },
+    title:{
+        flex:1,
+        justifyContent:'center',
+        flexDirection: 'row',
+        backgroundColor:'#5d5c5c',
+        paddingTop:5,
+        paddingBottom:5,
+        width:'100%',
+    },
+    description:{
+        backgroundColor:'white',
+        padding:5,
+        borderWidth:1,
+        marginLeft:20,
+        marginRight:20,
+    },
+    image:{
+        width:350,
+        height:300,
+    }
+
+})
 const Boats=({title, des, icon_name, poster})=>{
     return (
-        <View style={{ marginTop: 20}}>
-            <View style={{ flexDirection: 'row', }}>
-                <Icon name={icon_name} size={30} color="#707070" />
-                <Text style={{ color: '#707070', fontSize: 24, fontWeight: 'bold', marginLeft: 10 }}>
+        <View style={[styles.container,{ marginTop: 20}]}>
+            <View style={styles.title}>
+                <Icon name={icon_name} size={30} color="whitesmoke" />
+                <Text style={{ fontSize: 24, fontWeight: 'bold', marginLeft: 10, color:"whitesmoke" }}>
                     {title}
                 </Text>
             </View>
-            <Text style={{ marginTop: 10, marginBottom: 10}}>
+            <Text style={[styles.description,{ marginTop: 20, marginBottom: 20}]}>
                 {des}
             </Text>
-            <Image source={poster} style={{width:400,height:500}}/>
+            <Image source={poster} style={[styles.image]}/>
 
         </View>
     )
